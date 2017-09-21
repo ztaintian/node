6cd7468ef35abe2b742e5ef6b0c3bc678a0f81bd
ztiantian <243752773@qq.com>
2017-09-21 14:51:11 +0800

    nodedev

--- imgHttp/gethttpimg.js
+++ imgHttp/gethttpimg.js
@@ -1,28 +1,28 @@
 var http = require("http");
 var fs = require("fs");
-
-// var server = http.createServer(function(req, res){}).listen(50082);
-// console.log("http start");
+var path = require('path');
+var request = require('request');
 
  
 
 var url = "http://s0.hao123img.com/res/img/logo/logonew.png";
-http.get(url, function(res){
-    var imgData = "";
+request.get(url).pipe(fs.createWriteStream(path.join('./', 'aaa.jpg')));
+// http.get(url, function(res){
+//     var imgData = "";
 
-    res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
+//     res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
 
 
-    res.on("data", function(chunk){
-        imgData+=chunk;
-    });
+//     res.on("data", function(chunk){
+//         imgData+=chunk;
+//     });
 
-    res.on("end", function(){
-        fs.writeFile("logonew.png", imgData, "binary", function(err){
-            if(err){
-                console.log("down fail");
-            }
-            console.log("down success");
-        });
-    });
-});
+//     res.on("end", function(){
+//         fs.writeFile("logonew.png", imgData, "binary", function(err){
+//             if(err){
+//                 console.log("down fail");
+//             }
+//             console.log("down success");
+//         });
+//     });
+// });
