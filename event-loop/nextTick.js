311ce373cc1dea01ceddb05559a8c037f0a29655
unknown <zhutiantian@ciicsh.local>
2018-02-06 09:48:24 +0800

    kk

---
+++ event-loop/nextTick.js
@@ -0,0 +1,8 @@
+process.nextTick(function A() {
+  console.log(1);
+  process.nextTick(function B(){console.log(2);});
+});
+
+setTimeout(function timeout() {
+  console.log('TIMEOUT FIRED');
+}, 0)
